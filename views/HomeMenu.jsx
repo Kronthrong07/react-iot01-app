5a4d7b18d4fafd2760a8bd48f9ce7ffa3d0fca69
Kronthrong03 <s6419410007@sau.ac.th>
2025-04-01 11:05:26 +0700

    v2 แก้หน้าHomeMenu

--- views/HomeMenu.jsx
+++ views/HomeMenu.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar } from '@mui/material'
 
 function HomeMenu() {
   return (
-    <div>
-    <h1>NATTHAWUT</h1>
-    </div>
+    <>
+      <Box sx={{ height: '100vh', display: 'flex' }}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 3 }}>
+          <Avatar src={'https://st4.depositphotos.com/10866344/22175/v/450/depositphotos_221759756-stock-illustration-simple-icon-to-represent-the.jpg'} sx={{ width: 150, height: 150, mx: 'auto' }} />
+          <Typography variant='h3' sx={{ textAlign: 'center', mt: '2' }}>
+            IoT Calculate by Natt SAU
+          </Typography>
+        </Box>
+      </Box>
+    </>
   )
 }
 
